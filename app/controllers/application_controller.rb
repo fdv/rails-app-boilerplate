2ca9529b8a353be47dab09e596bc11f1e2ae51f4
Frédéric de Villamil <frederic@de-villamil.com>
2013-04-12 22:57:11 +0200

    Adds breadcrumb, page title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  add_breadcrumb :index, :root_path  
   
   def params
     @app ||= App.default
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def page_header(title)
+    content_tag(:div, :class => 'page-header') do
+      content_tag(:h1, title)
+    end    
+  end
+  
 end
