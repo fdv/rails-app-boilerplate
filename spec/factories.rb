bfed9ef5619c48057399c49292f9c20d18144316
Frédéric de Villamil <frederic@de-villamil.com>
2013-04-13 21:10:25 +0200

    Fixes build

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,5 +2,8 @@ FactoryGirl.define do
   factory :user do
     email "john@doe.com"
     password "toto42"
+    unconfirmed_email nil
+    confirmed_at "2013-04-13 18:28:12"
+    confirmation_sent_at "2013-04-13 18:25:28"
   end
 end
